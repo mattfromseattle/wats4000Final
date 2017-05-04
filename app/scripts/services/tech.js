6512be9d7c39566685db94b15b8663675b3a2b60
mattfromseattle <mattfromseattle@me.com>
2017-05-04 15:36:55 -0700

    Updated API call and positioning.

--- app/scripts/services/tech.js
+++ app/scripts/services/tech.js
@@ -11,11 +11,12 @@ angular.module('wats4000FinalApp')
   .factory('current', function ($resource) {
     // Service logic
     // ...
-    return $resource('https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey=2710c84ddcd04d2dbb72e814cb443962', {}, {
+    return $resource('https://newsapi.org/v1/articles?&apiKey=2710c84ddcd04d2dbb72e814cb443962', {}, {
       query: {
         method: 'GET',
         params: {
-          source: 'ars-technica'
+          source: 'ars-technica',
+          sortBy: 'latest'
         },
         isArray: false
       }
