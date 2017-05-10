cc58d5a47410e0b4642826a2ea55dd193eed9f79
Matt Dewey <mattfromseattle@me.com>
2017-05-10 15:32:55 -0700

    Added analytics.

--- app/scripts/services/tech.js
+++ app/scripts/services/tech.js
@@ -11,13 +11,28 @@ angular.module('wats4000FinalApp')
   .factory('current', function ($resource) {
     // Service logic
     // ...
-    return $resource('https://newsapi.org/v1/articles?source=:source&sortBy=:sortBy&apiKey=:apiKey', {}, {
+    return $resource('https://newsapi.org/v1/articles?source=:source&sortBy=:sortBy&apiKey=:apiKey&', {}, {
       query: {
         method: 'GET',
         params: {
-          source: 'reuters',
-          sortBy: 'latest',
+          source: 'bloomberg',
           apiKey: '2710c84ddcd04d2dbb72e814cb443962',
+        },
+        isArray: false
+      }
+    });
+  });
+
+  angular.module('wats4000FinalApp')
+  .factory('source', function ($resource) {
+    // Service logic
+    // ...
+    return $resource('https://newsapi.org/v1/sources?sortBy=:sortBy&apiKey=:apiKey&', {}, {
+      query: {
+        method: 'GET',
+        params: {
+          apiKey: '2710c84ddcd04d2dbb72e814cb443962',
+          sortBy: 'latest'
         },
         isArray: false
       }
